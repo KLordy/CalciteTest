ce3a52e260d88b997430448ae95bfdf1a2a42a30
klordy <zhonghao01@iqiyi.com>
2019-08-19 21:28:01 +0800

    init project

---
+++ src/main/java/org/klordy/promote/App.java
@@ -0,0 +1,13 @@
+package org.klordy.promote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
